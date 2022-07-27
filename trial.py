86c7a27db57bb88ece5ea2b2abfad83c31aee98a
miranda-liu <47536718+miranda-liu@users.noreply.github.com>
2022-07-26 21:33:02 -0700

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,2 +1,13 @@
-a = a + 1.0
-return a
+
+varA = -5
+varB = -10
+print(type(varA))
+
+if type(varA) == "str" or type(varB) == "str":
+    print("string involved")
+elif varA > varB:
+    print("bigger")
+elif varA == varB:
+  print("equal")
+elif varA < varB:
+  print("smaller")
