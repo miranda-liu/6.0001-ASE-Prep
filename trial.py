aa7fe65dacace4d8da8aed4a33925d04ca06978b
miranda-liu <47536718+miranda-liu@users.noreply.github.com>
2022-07-27 23:09:52 -0700

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,12 +1,90 @@
-varA = -5
-varB = -10
-print(type(varA))
+# varA = -5
+# varB = -10
+# print(type(varA))
 
-if type(varA) == "str" or type(varB) == "str":
-    print("string involved")
-elif varA > varB:
-    print("bigger")
-elif varA == varB:
-  print("equal")
-elif varA < varB:
-  print("smaller")
+# if type(varA) == "str" or type(varB) == "str":
+#     print("string involved")
+# elif varA > varB:
+#     print("bigger")
+# elif varA == varB:
+#   print("equal")
+# elif varA < varB:
+#   print("smaller")
+
+# divisor = 2
+# for num in range(0, 10, 2):
+#     print(num/divisor) 
+
+# school = 'Massachusetts Institute of Technology'
+# numVowels = 0
+# numCons = 0
+
+# for char in school:
+#     if char == 'a' or char == 'e' or char == 'i' \
+#        or char == 'o' or char == 'u':
+#         numVowels += 1
+#     elif char == 'o' or char == 'M':
+#         print(char)
+#     else:
+#         numCons -= 1
+
+# print('numVowels is: ' + str(numVowels))
+# print('numCons is: ' + str(numCons)) 
+
+# iteration = 0
+# count = 0
+# while iteration < 5:
+#     for letter in "hello, world":
+#         count += 1
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+#     iteration += 1 
+
+# for iteration in range(5):
+#     count = 0
+#     while True:
+#         for letter in "hello, world":
+#             count += 1
+#         print("Iteration " + str(iteration) + "; count is: " + str(count))
+#         break
+
+# iteration = 0
+# while iteration < 5:
+#     count = 0
+#     for letter in "hello, world":
+#         count += 1
+#         if iteration % 2 == 0:
+#             break
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+#     iteration += 1 
+
+# x = 25
+# epsilon = 0.01
+# step = 0.1
+# guess = 0.0
+
+# while guess <= x:
+#     if abs(guess**2 -x) >= epsilon:
+#         guess += step
+#     print(guess)
+#     break
+
+# if abs(guess**2 - x) >= epsilon:
+#     print('failed')
+# else:
+#     print('succeeded: ' + str(guess))
+
+x = 25
+epsilon = 0.01
+step = 0.1
+guess = 0.0
+
+while abs(guess**2-x) >= epsilon:
+    if guess <= x:
+        guess += step
+    else:
+        break
+
+if abs(guess**2 - x) >= epsilon:
+    print('failed')
+else:
+    print('succeeded: ' + str(guess))
